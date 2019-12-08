ff3a234e457e9b7ba67addba2957ade1dcf47bf6
Rananjaya <34897947+Rananjaya@users.noreply.github.com>
2019-12-08 14:59:29 +0530

    modifuations

--- App.js
+++ App.js
@@ -14,7 +14,8 @@ import {
   View,
   Text,
   StatusBar,
-  ActivityIndicator
+  ActivityIndicator,
+  FlatList
 } from 'react-native';
 
 import {
@@ -25,11 +26,53 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+const Url = 'https://facebook.github.io/react-native/movies.json'
 export default class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoding :true,
+      dataSource:null,
+    }
+  }
+
+  componentDidMount () {
+    return fetch(Url)
+    // console.log("Concat Values",Url.concat())
+        .then((response) => response.json()) //take the response from API and convert it in to json object
+        .then((responseJson) =>{
+             //we Have data now
+             console.log("We Have data",responseJson.movies);
+             this.setState({
+               isLoding:false,
+               dataSource:responseJson.movies,
+               
+             })
+        })
+        .catch((error) =>{
+            console.error(error);
+        });
+
+        console.log("State Data", this.state.dataSource)
+  
+  }
+
   render(){
+    if(this.state.isLoding){
+          return(
+            <View style={{flex:1,padding:20}}>
+              <ActivityIndicator />
+            </View>
+          )
+    }
     return(
       <View style={styles.container}>
-
+         <FlatList
+             data={this.state.dataSource}
+             renderItem={({item}) => <Text>{item.title}, {item.releaseYear}</Text>}
+             keyExtractor={item => item.id}
+         />
       </View>
     );
   }
